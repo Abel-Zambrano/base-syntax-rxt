34893006efbe205639edb57ebcd24b9a5ef6d862
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-20 11:14:34 -0700

    added event handler

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import UserOutput from './components/UserOutput';
 function App() {
   const [ usernameState, setUsernameState ] = useState('PizzaBoy2014');
 
+  const usernameHandler = (event) => {
+    setUsernameState(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>Assignment 1</h1>
       <UserInput />
       <UserOutput username1="PrincessChloe2017" username2={usernameState} />
+      <UserOutput username1="Example 1" username2="Example 2" />
     </div>
   );
 }
