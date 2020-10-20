bcb0fca5d5de0e22b335702f922209bc194f398a
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-20 11:31:55 -0700

    added styling

--- src/components/UserOutput.jsx
+++ src/components/UserOutput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserOutput = (props) => {
     return (
-        <div>
+        <div className='userOutput'>
             <p>{props.username1}</p>
             <p>{props.username2}</p>
         </div>
